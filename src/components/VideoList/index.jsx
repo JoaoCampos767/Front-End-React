36cab57181c2e8f025578da2100185253538cdb7
JoaoCamposMBM <joao.pedro@mbmsolutions.com.br>
2022-09-19 14:05:33 -0300

    Terminando link

--- src/components/VideoList/index.jsx
+++ src/components/VideoList/index.jsx
@@ -2,11 +2,18 @@ import React from "react";
 
 import { Container, VideoListWrapper } from "./styles";
 import AddVideo from "../AddVideo";
+import Video from "../Video";
 
 export default function VideoList() {
   return (
     <Container>
       <VideoListWrapper>
+        <Video
+          id={1}
+          title="Title test"
+          link="https://www.youtube.com/watch?v=ExNSl9CKcPQ&list=PL8YNlUoOZkkY8wjaI2t8DfvysoysmMLCv&index=3"
+          liked={false}
+        />
         <AddVideo />
       </VideoListWrapper>
     </Container>
